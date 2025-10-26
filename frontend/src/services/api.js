f4461f3a693e0799036f59e5d73b45bc3166fa25
Sri Chaithanya Karri <srichaithanyakarri30@gmail.com>
2025-10-26 20:14:55 +0530

    Use environment variable for API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
 
 // Generate a new quiz from Wikipedia URL
 export const generateQuiz = async (url, force = false) => {
