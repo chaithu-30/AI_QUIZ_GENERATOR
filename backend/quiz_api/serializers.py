739db69b2b7260aa699dafd366ab060e3d053ff1
Sri Chaithanya Karri <srichaithanyakarri30@gmail.com>
2025-10-29 17:16:05 +0530

    Clean up docstrings in serializers
    Removed unnecessary docstrings from QuizSerializer and QuizListSerializer.

--- backend/quiz_api/serializers.py
+++ backend/quiz_api/serializers.py
@@ -3,9 +3,6 @@ from .models import Quiz
 import json
 
 class QuizSerializer(serializers.ModelSerializer):
-    """Serializer for the Quiz model"""
-    
-    # Parse full_quiz_data JSON when returning data
     full_quiz_data = serializers.SerializerMethodField()
     
     class Meta:
@@ -14,15 +11,12 @@ class QuizSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'date_generated']
     
     def get_full_quiz_data(self, obj):
-        """Parse the JSON string into a Python dict"""
         if obj.full_quiz_data:
             return json.loads(obj.full_quiz_data)
         return None
 
 
 class QuizListSerializer(serializers.ModelSerializer):
-    """Simplified serializer for history list"""
-    
     class Meta:
         model = Quiz
         fields = ['id', 'url', 'title', 'date_generated']
